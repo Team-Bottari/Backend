e2dacdeb0321f6914b573088a5fac1f90fbe7f14
wisixicidi <wisixicidi@gmail.com>
2023-02-25 14:18:24 +0900

    async db update

---
+++ async_db/database.py
@@ -0,0 +1,20 @@
+from settings import HOST, DB_USER, DB_PASSWORD, DB_PORT, DATABASE
+from sqlalchemy.ext.asyncio import create_async_engine,AsyncSession
+from sqlalchemy.orm import sessionmaker
+
+# DB_URL = f'mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{HOST}:{DB_PORT}/{DATABASE}'
+DB_URL = f'mysql+aiomysql://{DB_USER}:{DB_PASSWORD}@{HOST}:{DB_PORT}/{DATABASE}'
+
+class engineconn:
+
+    def __init__(self):
+        self.engine = create_async_engine(DB_URL, pool_recycle = 500)
+
+    def sessionmaker(self):
+        Session = sessionmaker(bind=self.engine,class_=AsyncSession)
+        session = Session()
+        return session
+
+    def connection(self):
+        conn = self.engine.connect()
+        return conn
--- source/member/member_source.py
+++ source/member/member_source.py
@@ -4,7 +4,8 @@ from fastapi import UploadFile, File, Request
 from fastapi.encoders import jsonable_encoder
 from config import MEMBER_URL
 from fastapi_utils.inferring_router import InferringRouter
-from db.database import engineconn
+# from db.database import engineconn
+from async_db.database import engineconn
 import os
 from .member_data import Member_signup
 from db.models import Member
@@ -12,13 +13,15 @@ import datetime
 
 member_router = InferringRouter()
 
+# engine = engineconn()
+# session = engine.sessionmaker()
 engine = engineconn()
 session = engine.sessionmaker()
 
 @cbv(member_router)
 class MemberSource:
     @member_router.post(MEMBER_URL+"/sign_up")
-    def sign_up(self, member_info: Member_signup):
+    async def sign_up(self, member_info: Member_signup):
 
         # birth date로 형변환
         format = '%Y/%m/%d'
@@ -27,13 +30,13 @@ class MemberSource:
         # 회원가입
         member = Member(**member_info.dict())
         session.add(member)
-        session.commit()
+        await session.commit()
         
         return member
 
 
     @member_router.post(MEMBER_URL+"/image_upload")
-    def image_upload(self, profile_image: UploadFile=File()):
+    async def image_upload(self, profile_image: UploadFile=File()):
         print(profile_image)
         return profile_image
 
