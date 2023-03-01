a0170bde188cb75aa3d79033e18753f94e346e66
lee nam kyeong <skarod0401@naver.com>
2023-03-01 15:13:10 +0900

    로그인

--- source/member/member_source.py
+++ source/member/member_source.py
@@ -5,8 +5,8 @@ from fastapi.encoders import jsonable_encoder
 from config import MEMBER_URL
 from fastapi_utils.inferring_router import InferringRouter
 from .member_data import Member_signup,Member_override,Member_login,Member_changepw,Member_findpw,Member_findemail, Member_info_check
-from async_db import session,Member
-from sqlalchemy import select
+from async_db import session, Member
+from sqlalchemy import select, update
 import datetime
 
 member_router = InferringRouter()
@@ -19,7 +19,7 @@ class MemberSource:
         member_info = jsonable_encoder(member_info)
         query = select(Member).where(Member.id==member_info["id"])
         result = await session.execute(query)
-        if result.first() is None:
+        if result is None:
             return {"override":False}
         else:
             return {"override":True}
@@ -28,7 +28,7 @@ class MemberSource:
     async def sign_up(self, member_info: Member_signup):
         member_info = jsonable_encoder(member_info)
         # birth date로 형변환
-        member_info['birth'] = datetime.datetime.strptime(member_info['birth'],'%Y/%m/%d')
+        member_info['birth'] = datetime.datetime.strptime(member_info['birth'],'%Y-%m-%d')
         member_info['create_at'] = datetime.datetime.now()
         # 회원가입
         member = Member(**member_info)
@@ -42,10 +42,15 @@ class MemberSource:
         member_info = jsonable_encoder(member_info)
         query = select(Member).where(Member.id==member_info["id"], Member.pw == member_info['pw'])
         result = await session.execute(query)
-        #TODO 마지막 접속시간 체크?
-        if result.first() is None:
+        
+        if result is None:
             return {"sign_in": False}
         else:
+            # 최근 로그인한 시간 체크
+            print(datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
+            query = update(Member).where(Member.id==member_info["id"]).values(last_login=datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
+            result = await session.execute(query)
+            await session.commit()
             return {"sign_in": True}
 
     @member_router.post(MEMBER_URL+"/logout",summary="로그아웃",)
@@ -70,6 +75,14 @@ class MemberSource:
     @member_router.post(MEMBER_URL+"/id-find",summary="이메일찾기",)
     async def find_email(self,member_info:Member_findemail):
         member_info = jsonable_encoder(member_info)
+        query = select(Member).where(Member.name==member_info["name"]).where(Member.birth==member_info["birth"])
+        result = await session.execute(query)
+        info = result.first()
+        if info is None:
+            return JSONResponse({"id":False})
+        else:
+            info = jsonable_encoder(info[0])
+            return JSONResponse({'id':info["id"]})
         
     
     @member_router.post(MEMBER_URL+"/pw-find",summary="비밀번호찾기",)
