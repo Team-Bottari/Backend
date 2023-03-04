ddf3dab36e26d021c012bb348246006890b8a142
wisixicidi <wisixicidi@gmail.com>
2023-03-04 13:35:46 +0900

    .

--- utils.py
+++ utils.py
@@ -62,27 +62,29 @@ async def response_parse(response):
 def make_log(start_log,end_log,start_time,end_time):
     return start_log+"\n"+end_log+f"\n\t처리시간 : {str(end_time-start_time)[:7]}"
 
-def make_html(key):
-    # HTML = f"""
-    # <html>
-    #     <head>
-    #         <meta charset='utf-8'>
-    #         <meta http-equiv='X-UA-Compatible' content='IE=edge'>
-    #         <title>Page Title</title>
-    #         <meta name='viewport' content='width=device-width, initial-scale=1'>
-    #         <link rel='stylesheet' type='text/css' media='screen' href='main.css'>
-    #         <script src='main.js'></script>
-    #     </head>
-    # <body>
-    #     <div>
-    #         <h3>안녕하세요 GYM-Bottari 입니다.</h3>
-    #     </div>
-    #     <p>요청에 동의하시면 아래 버튼을 눌러주세요.</p>
-    #     <form action="http://{DDNS}:{PORT}/api/{VERSION}/certification/{key}",method="get">
-    #         <button type>인증에 동의합니다.</button>
-    #     </form>
-    # </body>
-    # </html>"""
+def make_certificate_html(key):
+    HTML = f"""
+    <html>
+        <head>
+            <meta charset='utf-8'>
+            <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+            <title>Page Title</title>
+            <meta name='viewport' content='width=device-width, initial-scale=1'>
+            <link rel='stylesheet' type='text/css' media='screen' href='main.css'>
+            <script src='main.js'></script>
+        </head>
+    <body>
+        <div>
+            <h3>안녕하세요 GYM-Bottari 입니다.</h3>
+        </div>
+        <p>요청에 동의하시면 아래 버튼을 눌러주세요.</p>
+        <form action="http://{DDNS}:{PORT}/api/{VERSION}/certification/{key}",method="get">
+            <button type>인증에 동의합니다.</button>
+        </form>
+    </body>
+    </html>"""
+
+def make_passwd_html(key):
     HTML = f"""
     <html>
         <head>
@@ -102,6 +104,25 @@ def make_html(key):
     </html>"""
     return HTML
 
+def send_certificate_email(recvEmail,key):
+    sendEmail = EMAILS[0]["email"]
+    password = EMAILS[0]["password"]
+    smtpName = EMAILS[0]["smtp"]
+    smtpPort = EMAILS[0]["port"]
+    message = MIMEMultipart("alternative")
+    message['Subject'] ="안녕하세요. GYM-Bottari 입니다."
+    message['From'] = sendEmail
+    message['To'] = recvEmail
+    html = make_certificate_html(key)
+    part = MIMEText(html , "html")
+    message.attach(part)
+    s=smtplib.SMTP( smtpName , smtpPort ) #메일 서버 연결
+    s.starttls() #TLS 보안 처리
+    s.login( sendEmail , password ) #로그인
+    s.sendmail( sendEmail, recvEmail, message.as_string()) #메일 전송, 문자열로 변환하여 보냅니다.
+    s.close() #smtp 서버 연결을 종료합니다.
+    
+
 def send_pw_mail(recvEmail,key):
     sendEmail = EMAILS[0]["email"]
     password = EMAILS[0]["password"]
@@ -111,7 +132,7 @@ def send_pw_mail(recvEmail,key):
     message['Subject'] ="안녕하세요. GYM-Bottari 입니다."
     message['From'] = sendEmail
     message['To'] = recvEmail
-    html = make_html(key)
+    html = make_passwd_html(key)
     part = MIMEText(html , "html")
     message.attach(part)
     s=smtplib.SMTP( smtpName , smtpPort ) #메일 서버 연결
