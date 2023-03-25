1569709ec6e89fce67b353d282867a6ac36e6fd0
lee nam kyeong <skarod0401@naver.com>
2023-03-25 12:45:23 +0900

    positng member id -> email로 변경

--- source/posting/posting_source.py
+++ source/posting/posting_source.py
@@ -14,7 +14,7 @@ class PostingSource:
     @posting_router.post(POSTING_URL)
     async def create_posting(self,posting:Posting_create):
         posting = jsonable_encoder(posting)
-        query = select(Member).where(Member.id==posting["member_id"])
+        query = select(Member).where(Member.email==posting["member_id"])
         result = await session.execute(query)
         member_id = jsonable_encoder(result.first()[0])["member_id"]
         posting_info = await update_poting_create(posting,member_id)
